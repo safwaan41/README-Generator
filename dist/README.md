84dcb1f6168fc234ac2dc348b3859620bde5f05b
safwaan41 <safwaan41@gmail.com>
2023-07-14 19:29:54 -0500

    readme generate working need to add more questions

---
+++ dist/README.md
@@ -0,0 +1,22 @@
+
+# ReadmeGenerator
+
+## Table Of Contents
+- [Description](#description)
+- [Use](#use)
+- [License](#license)
+- [Questions](#questions)
+## Description
+it is cool
+
+## Use
+make a readme
+
+## License
+MIT
+
+## Questions
+If you have any questions, you can reach me at https://github.com/safwaan41
+
+
+    
--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        {
+            name: `projectName`,
+            type: `input`,
+            message: `What is the name of your project?`,
+        },
+        {
+            name: `description`,
+            type: `input`,
+            message: `Write a breif description of your project...`,
+        },
+        {
+            name: `use`,
+            type: `input`,
+            message: `What is the best use for your project?`,
+        },
+        {
+            name: `license`,
+            type: `list`,
+            message: `Do you need a license for your project?`,
+            choices: [`MIT`, `Berkeley Software Distribution (BSD)`, `Apache 2.0`, `None`]
+        },
+        {
+            name: `github`,
+            type: `input`,
+            message: `What is the Github username?`,
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+    
+];
 
-// Function call to initialize app
-init();
+inquirer.prompt(questions)
+.then(answers=>{
+    fs.writeFileSync("./dist/README.md",`
+# ${answers.projectName}
+
+## Table Of Contents
+- [Description](#description)
+- [Use](#use)
+- [License](#license)
+- [Questions](#questions)
+## Description
+${answers.description}
+
+## Use
+${answers.use}
+
+## License
+${answers.license}
+
+## Questions
+If you have any questions, you can reach me at https://github.com/${answers.github}
+
+
+    `)
+})
